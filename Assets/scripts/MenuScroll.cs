05c7bee03f325ce9e8d373c65e660f6442e020bd
RobotGandhi <99viktor.vc@gmail.com>
2019-03-01 15:30:17 +0100

    Basic level menu

---
+++ Assets/scripts/MenuScroll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScroll : MonoBehaviour
+{
+    public ScrollRect scrollRect;
+    public Scrollbar scrollBar;
+    public Image image;
+
+    void OnEnable()
+    {
+        //Subscribe to the ScrollRect event
+        scrollRect.onValueChanged.AddListener(scrollRectCallBack);
+        scrollBar.value = 1f;
+    }
+
+    //Will be called when ScrollRect changes
+    void scrollRectCallBack(Vector2 value)
+    {
+        Debug.Log("ScrollRect Changed: " + value);
+        
+        image.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, image.sprite.rect.height * (value.y - 1) * -1, 0);
+        Debug.Log(image.gameObject.transform.position);
+    }
+
+    void OnDisable()
+    {
+        //Un-Subscribe To ScrollRect Event
+        scrollRect.onValueChanged.RemoveListener(scrollRectCallBack);
+    }
+}
--- Assets/scripts/menu.cs
+++ Assets/scripts/menu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
+public class menu : MonoBehaviour
 {
     
     private void Update()
